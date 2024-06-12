5baadc811b03032b42c47b7c4bd854e357235125
Avinash <avinash.kushwaha1501@gmail.com>
2024-06-12 18:03:29 +0000

    RECOIL Functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
-import react, { useState } from 'react'
+import react from 'react'
+import { RecoilRoot } from 'recoil'
+import { useRecoilValue } from 'recoil'
+import { JobsAtom, NetworkAtom ,MessagingAtom,NotificationAtom} from './Atoms'
+
 
 function App(){
-  const [count,setcount]=useState(0)
-
   return(
-    <>
+    <RecoilRoot>
+      <MainApp/>
+    </RecoilRoot>
+  )
+}
+function MainApp(){
+  const NetworkCount = useRecoilValue(NetworkAtom)
+  const JobsCount = useRecoilValue(JobsAtom)
+  const MessagingCount = useRecoilValue(MessagingAtom)
+  const NotificationCount = useRecoilValue(NotificationAtom)
+  return(
+    <>    
+    <button> Home </button>
+    
+    <button> My Network ({ NetworkCount >= 100 ? "99+" : NetworkCount }) </button>
+    <button> Jobs ({JobsCount}) </button>
+    <button> Messaging ({MessagingCount}) </button>
+    <button> Notifications ({NotificationCount}) </button>
+    
+    <button> Me </button>
     
     </>
   )
+
 }
+
+
 export default App
---
+++ src/Atoms.js
@@ -0,0 +1,18 @@
+import { atom } from "recoil";
+
+export const NetworkAtom=atom({
+    key:"NetworkAtom",
+    default:10
+})
+export const NotificationAtom=atom({
+    key:"NotificationAtom",
+    default:12
+})
+export const JobsAtom=atom({
+    key:"JobsAtom",
+    default:12
+})
+export const MessagingAtom=atom({
+    key:"MessagingAtom",
+    default:12
+})
